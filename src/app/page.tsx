b4c3f69256c9e730f2882282f4e71858d15928e7
Dimas Roger <53912205+rogersovich@users.noreply.github.com>
2023-07-01 12:40:47 +0700

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,18 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image"
+import styles from "./page.module.css"
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+        <p>My Name is Dimas Roger</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
